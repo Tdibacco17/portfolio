9308623ea7b04a0fe81137764aeaf2e6a8cc8fef
Tdibacco17 <tomasdibacco@gmail.com>
2024-07-08 16:39:07 -0300

    hotfix | icons in safari

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env:{
+        BASE_PATH: "http://localhost:3000/"
+    }
+    /* config options here */
+}
+
+module.exports = nextConfig
